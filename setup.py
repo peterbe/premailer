3981359de1ce974b24f69a8a99bb033e55ec3230
Peter Bengtsson <mail@peterbe.com>
2013-03-21 14:38:22 -0700

    version 1.13

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.12'
+version = '1.13'
 
 README = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(README).read().strip() + "\n\n"
