583a45973e5240a259916336e300764db0f19571
Ryan Butterfield <git@ryanbutterfield.com>
2013-10-13 20:18:14 +1100

    PEP8 spacing.

--- premailer/premailer.py
+++ premailer/premailer.py
@@ -75,11 +75,13 @@ def merge_styles(old, new, class_=''):
                                               in mergeable)))
         return ' '.join(x for x in all if x != '{}')
 
+
 def make_important(bulk):
     """makes every property in a string !important.
     """
     return ';'.join('%s !important' % p if not p.endswith('!important') else p 
                     for p in bulk.split(';'))
+
 
 _css_comments = re.compile(r'/\*.*?\*/', re.MULTILINE | re.DOTALL)
 _regex = re.compile('((.*?){(.*?)})', re.DOTALL | re.M)
