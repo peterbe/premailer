1532c7489a3699b5d9b42e222e3faf704421970e
Sandeep Aggarwal <asandeep.me@gmail.com>
2019-07-06 15:34:40 +0530

    Added lock for cache access.

--- premailer/cache.py
+++ premailer/cache.py
@@ -1,15 +1,18 @@
 import functools
 import os
+import threading
 
 from cachetools import cached, LRUCache
 
-
 cache = LRUCache(maxsize=int(os.environ.get("PREMAILER_CACHE_MAXSIZE", 128)))
+
+# Lock to prevent multiple threads from accessing the cache at same time.
+CACHE_ACCESS_LOCK = threading.RLock()
 
 
 def function_cache(**options):
     def decorator(func):
-        @cached(cache)
+        @cached(cache, lock=CACHE_ACCESS_LOCK)
         @functools.wraps(func)
         def inner(*args, **kwargs):
             return func(*args, **kwargs)
