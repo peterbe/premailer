49cb15709f33bab81acfcfc6076531fb61d5658a
Mariano Benedettini <mariano@codexia.io>
2019-08-22 22:08:01 -0300

    Pass validate to csstext_to_pairs

--- premailer/premailer.py
+++ premailer/premailer.py
@@ -445,7 +445,7 @@ class Premailer(object):
             items = sel(page)
             if len(items):
                 # same so process it first
-                processed_style = csstext_to_pairs(style)
+                processed_style = csstext_to_pairs(style, validate=not self.disable_validation)
 
                 for item in items:
                     item_id = id(item)
